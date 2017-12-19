5d71cd85790fac4cb216745feba99127526dd893
owen <owen@owens-MacBook-Pro.local>
2017-12-19 23:30:01 +0800

    ff

---
+++ index.php
@@ -0,0 +1,27 @@
+﻿<!doctype html>
+<html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="Generator" content="EditPlus®">
+  <meta name="Author" content="">
+  <meta name="Keywords" content="">
+  <meta name="Description" content="">
+  <title>Document</title>
+<style>
+body{margin:0;}
+.wrap{margin:0 200px;}
+  .webtitle{font-size: 40px;font-weight: bold;padding:20px 0;padding-left:20px;background:#eee; }
+  .main{text-align:center;}
+</style>
+ </head>
+ <body>
+   <div class="wrap">
+   <div class="webtitle">
+     <?php 
+     echo "Jiaxianghua";
+     ?>
+   </div>
+  <div class="main"><img src="/images/chinamap.png"></div>
+     </div>
+ </body>
+</html>
